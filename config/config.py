ef3a81889c4f1404dcd6c21d7937f829bc1df298
Oleh <oleguzva3@gmail.com>
2025-05-09 21:58:12 +0300

    first commit

---
+++ config/config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN_BOT = os.getenv('TELEGRAM_TOKEN_BOT')
+ADMIN_ID = os.getenv('ADMIN_ID')
+DATABASE_URL = os.getenv('DATABASE_URL')
